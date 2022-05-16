f4196dcda768b5b68b32a55cfef71465d1bc4c9d
bao <lanchinhaicon@gmail.com>
2022-05-16 12:53:01 +0100

    Updates

---
+++ precache-manifest.5a27a34514b7e0addfc20e90a1cf61d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6826d9fb3e13a9360b0951e0c63b9296",
+    "url": "/infiniteScroll/index.html"
+  },
+  {
+    "revision": "66036cdc9183d865c3e6",
+    "url": "/infiniteScroll/static/css/main.fcd1b2fb.chunk.css"
+  },
+  {
+    "revision": "252eced4bdb89a79c53d",
+    "url": "/infiniteScroll/static/js/2.64715bce.chunk.js"
+  },
+  {
+    "revision": "66036cdc9183d865c3e6",
+    "url": "/infiniteScroll/static/js/main.e0672df7.chunk.js"
+  },
+  {
+    "revision": "b3bf5e27869cbf1035a4",
+    "url": "/infiniteScroll/static/js/runtime-main.12cb0aa7.js"
+  }
+]);
